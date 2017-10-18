271af0a2daca494170af9089a86e8b0974f4951e
Clayton Pabst <claytonpabst@gmail.com>
2017-10-17 20:35:58 -0600

    updated header

--- src/components/Headers/MainHeader.js
+++ src/components/Headers/MainHeader.js
@@ -19,8 +19,8 @@ class MainHeader extends Component {
     return (
       <div className="mainHeaderWrapper">
         <div style={{"width":"230px"}} className='flexRow'>
-            <img style={{"height":"40px"}} src="https://static1.squarespace.com/static/57684221579fb3ab7144f54d/57ee4f38893fc0c995dad73a/57ee4f39d1758e241444feb2/1475235642440/native+instruments.png" alt=""/>
-            <ul style={headerName} >Native Instruments</ul>
+            <img className='aLink' style={{"height":"40px"}} src="https://cdn.rekkerd.org/wp-content/uploads/2017/03/Native-Instruments-logo.png" alt=""/>
+            {/*<ul style={headerName} >Native Instruments</ul>*/}
         </div>
         <div style={{"width":"500px"}} className='flexRow'>
             <ul className='navTextMedium'>PRODUCTS</ul>
@@ -29,9 +29,9 @@ class MainHeader extends Component {
             <ul className='navTextMedium'>SUPPORT</ul>
         </div>
         <div style={{"width":"150px"}} className='flexRow'>
-            <img style={{"height":"20px"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQETV_iGZaujVjXGEEhzClQMErGjomXPTr7XfTj_qIltNDzqUwmAQ" alt=""/>
-            <img style={{"height":"20px"}} src="https://d30y9cdsu7xlg0.cloudfront.net/png/5641-200.png" alt=""/>
-            <img style={{"height":"20px"}} src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//search1600.png" alt=""/>
+            <img className='aLink' style={{"height":"20px"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQETV_iGZaujVjXGEEhzClQMErGjomXPTr7XfTj_qIltNDzqUwmAQ" alt=""/>
+            <img className='aLink' style={{"height":"20px"}} src="https://d30y9cdsu7xlg0.cloudfront.net/png/5641-200.png" alt=""/>
+            <img className='aLink' style={{"height":"20px"}} src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//search1600.png" alt=""/>
         </div>
       </div>
     );
