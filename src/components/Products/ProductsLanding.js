7dcd1745161320070f50bed7850ed38694201f45
Clayton Pabst <claytonpabst@gmail.com>
2017-10-19 20:30:41 -0600

    ProductsLanding components has a header

---
+++ src/components/Products/ProductsLanding.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import MainHeader from './../Headers/MainHeader.js';
+
+
+class ProductsLanding extends Component {
+
+  render() {
+    return (
+      <div className="home">
+
+          <MainHeader/>
+
+      </div>
+    );
+  }
+}
+
+
+export default ProductsLanding;
