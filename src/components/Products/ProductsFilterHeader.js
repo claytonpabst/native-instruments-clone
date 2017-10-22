0afb3acf57a70509ae2e0c18c9ed0e1cda180a4c
Clayton Pabst <claytonpabst@gmail.com>
2017-10-21 18:04:23 -0600

    config made....

--- src/components/Products/ProductsFilterHeader.js
+++ src/components/Products/ProductsFilterHeader.js
@@ -11,6 +11,7 @@ class ProductsFilterHeader extends Component {
             itemColors:['#19f','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc','#ccc']
         }
         //bind shit here
+        this.updateItemColors = this.updateItemColors.bind(this);
     }
 
     updateItemColors(num, val){
@@ -33,21 +34,21 @@ class ProductsFilterHeader extends Component {
       <section className="pfhSection">
         <div className='pfhContentWrapper'>
             <ul style={{"flexWrap":"wrap"}} className='flexRow'>
-                <li onClick={ ()=> { this.updateItemColors(0, 'All') }} style={{"color":`${this.state.itemColors[0]}`}}>All</li>
-                <li onClick={ ()=> { this.updateItemColors(1, 'Free') }} style={{"color":`${this.state.itemColors[1]}`}}>Free</li>
-                <li onClick={ ()=> { this.updateItemColors(2, 'Bundles') }} style={{"color":`${this.state.itemColors[2]}`}}>Bundles</li>
-                <li onClick={ ()=> { this.updateItemColors(3, 'Keyboards') }} style={{"color":`${this.state.itemColors[3]}`}}>Keyboards</li>
-                <li onClick={ ()=> { this.updateItemColors(4, 'Synths') }} style={{"color":`${this.state.itemColors[4]}`}}>Synths</li>
-                <li onClick={ ()=> { this.updateItemColors(5, 'Samplers') }} style={{"color":`${this.state.itemColors[5]}`}}>Samplers</li>
-                <li onClick={ ()=> { this.updateItemColors(6, 'Effects') }} style={{"color":`${this.state.itemColors[6]}`}}>Effects</li>
-                <li onClick={ ()=> { this.updateItemColors(7, 'Drums') }} style={{"color":`${this.state.itemColors[7]}`}}>Drums</li>
-                <li onClick={ ()=> { this.updateItemColors(8, 'Keys') }} style={{"color":`${this.state.itemColors[8]}`}}>Keys</li>
-                <li onClick={ ()=> { this.updateItemColors(9, 'Guitars') }} style={{"color":`${this.state.itemColors[9]}`}}>Guitars</li>
-                <li onClick={ ()=> { this.updateItemColors(10, 'Strings') }} style={{"color":`${this.state.itemColors[10]}`}}>Strings</li>
-                <li onClick={ ()=> { this.updateItemColors(11, 'Brass/Woodwinds') }} style={{"color":`${this.state.itemColors[11]}`}}>Brass/Woodwinds</li>
-                <li onClick={ ()=> { this.updateItemColors(12, 'Cinematic') }} style={{"color":`${this.state.itemColors[12]}`}}>Cinematic</li>
-                <li onClick={ ()=> { this.updateItemColors(13, 'World') }} style={{"color":`${this.state.itemColors[13]}`}}>World</li>
-                <li onClick={ ()=> { this.updateItemColors(14, 'Audio Interfaces') }} style={{"color":`${this.state.itemColors[14]}`}}>Audio Interfaces</li>
+                <li onClick={ ()=> { this.updateItemColors(0, 'All') }}                 style={{"color":`${this.state.itemColors[0]}`}}>All</li>
+                <li onClick={ ()=> { this.updateItemColors(1, 'Free') }}                style={{"color":`${this.state.itemColors[1]}`}}>Free</li>
+                <li onClick={ ()=> { this.updateItemColors(2, 'Bundles') }}             style={{"color":`${this.state.itemColors[2]}`}}>Bundles</li>
+                <li onClick={ ()=> { this.updateItemColors(3, 'Keyboards') }}           style={{"color":`${this.state.itemColors[3]}`}}>Keyboards</li>
+                <li onClick={ ()=> { this.updateItemColors(4, 'Synths') }}              style={{"color":`${this.state.itemColors[4]}`}}>Synths</li>
+                <li onClick={ ()=> { this.updateItemColors(5, 'Samplers') }}            style={{"color":`${this.state.itemColors[5]}`}}>Samplers</li>
+                <li onClick={ ()=> { this.updateItemColors(6, 'Effects') }}             style={{"color":`${this.state.itemColors[6]}`}}>Effects</li>
+                <li onClick={ ()=> { this.updateItemColors(7, 'Drums') }}               style={{"color":`${this.state.itemColors[7]}`}}>Drums</li>
+                <li onClick={ ()=> { this.updateItemColors(8, 'Keys') }}                style={{"color":`${this.state.itemColors[8]}`}}>Keys</li>
+                <li onClick={ ()=> { this.updateItemColors(9, 'Guitars') }}             style={{"color":`${this.state.itemColors[9]}`}}>Guitars</li>
+                <li onClick={ ()=> { this.updateItemColors(10, 'Strings') }}            style={{"color":`${this.state.itemColors[10]}`}}>Strings</li>
+                <li onClick={ ()=> { this.updateItemColors(11, 'Brass/Woodwinds') }}    style={{"color":`${this.state.itemColors[11]}`}}>Brass/Woodwinds</li>
+                <li onClick={ ()=> { this.updateItemColors(12, 'Cinematic') }}          style={{"color":`${this.state.itemColors[12]}`}}>Cinematic</li>
+                <li onClick={ ()=> { this.updateItemColors(13, 'World') }}              style={{"color":`${this.state.itemColors[13]}`}}>World</li>
+                <li onClick={ ()=> { this.updateItemColors(14, 'Audio Interfaces') }}   style={{"color":`${this.state.itemColors[14]}`}}>Audio Interfaces</li>
             </ul>
         </div>
       </section>
