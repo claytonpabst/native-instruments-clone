3cc6f3042998f3642fa4935650982ec879e88df7
Clayton Pabst <claytonpabst@gmail.com>
2017-10-25 04:04:52 -0600

    login is dynamic

--- src/components/Login/LoginLanding.js
+++ src/components/Login/LoginLanding.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MainHeader from './../Headers/MainHeader.js';
 
 import './LoginLanding.css';
 
@@ -9,25 +11,47 @@ class LoginLanding extends Component {
     super(props)
 
     this.state = {
-
+      usernameInput: '',
+      passwordInput: ''
     }
     //bind shit here
     this.login = this.login.bind(this);
+    this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
   }
 
   login(){
     console.log('hit')
-    axios.post(`/api/login`, {"username":'user', "userpassword":'user'})
+    axios.post(`/api/login`, {"username":this.state.usernameInput, "userpassword":this.state.passwordInput})
     .then( res => {
-        console.log(res)
-    })        
+      if(res.data.length){
+        alert('Logged in as ' + res.data[0].username)
+      }
+    })   
+  }
+
+  handleUsernameInput(e){
+    this.setState({
+      usernameInput: e.target.value
+    })
+  }
+  handlePasswordInput(e){
+    this.setState({
+      passwordInput: e.target.value
+    })
   }
 
   render() {
-
+    console.log(this.state);
     return (
       <section className="">
-        <button onClick={this.login}></button>
+        <MainHeader/>
+        <h1>Enter Username</h1>
+        <input onChange={this.handleUsernameInput} type="text"/>
+        <br/>
+        <h1>Enter Password</h1>
+        <input onChange={this.handlePasswordInput} type="text"/>
+        <Link to='/'><button onClick={this.login}>Login</button></Link>
       </section>
     );
   }
--- src/components/Products/ProductsLandingProducts.js
+++ src/components/Products/ProductsLandingProducts.js
@@ -5,65 +5,65 @@ import './ProductsLandingProducts.css';
 
 
 class ProductsLandingProducts extends Component {
-  constructor(props){
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-        products:[]
+        this.state = {
+            products: []
+        }
+        //bind shit here
+        this.addToCart = this.addToCart.bind(this);
     }
-    //bind shit here
-    this.addToCart = this.addToCart.bind(this);
-  }
 
-  componentDidMount(){
-    axios.get(`/api/getProducts`)
-    .then( res => {
-        console.log(res)
-        this.setState({
-            products: res.data
-        })
-    })        
-  }
+    componentDidMount() {
+        axios.get(`/api/getProducts`)
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    products: res.data
+                })
+            })
+    }
 
-  addToCart(productID){
-      axios.post('/api/addToCart', {"productID":productID})
-      .then(res =>{
-          console.log(res);
-      })
-  }
+    addToCart(productID, productTitle) {
+        axios.post('/api/addToCart', { "productID": productID })
+            .then(res => {
+                alert(productTitle + ' was added to your cart.');
+            })
+    }
 
-  render() {
-    let products;
-        if(this.state.products.length){
+    render() {
+        let products;
+        if (this.state.products.length) {
             products = this.state.products.map((product, i) => {
-                if(product.attributes.indexOf(this.props.attributeToShow) > -1){
-                    return(
+                if (product.attributes.indexOf(this.props.attributeToShow) > -1) {
+                    return (
                         <div className='plpSingleProductWrapper' key={i}>
-                            <img src={product.image} alt=""/>
+                            <img src={product.image} alt="" />
                             <h1>{product.title}</h1>
                             <p>{product.description}</p>
                             <h3>{product.price}</h3>
-                            <h5 onClick={() => this.addToCart(product.id)}>Buy Now</h5>
+                            <h5 onClick={() => this.addToCart(product.id, product.title)}>Buy Now</h5>
                         </div>
                     )
                 }
             })
-        }else{
+        } else {
             products =
                 <div className='plpSingleProductWrapper'>
                     <h1>No 'products' in the database were found</h1>
                 </div>
-                   
+
         }
 
-    return (
-      <section className="plpSection">
-        <div className='plpContentWrapper'>
-            {products}
-        </div>
-      </section>
-    );
-  }
+        return (
+            <section className="plpSection">
+                <div className='plpContentWrapper'>
+                    {products}
+                </div>
+            </section>
+        );
+    }
 }
 
 
