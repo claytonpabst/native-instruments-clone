bcf5b8ce0bccf00e1bb4b78f31b9215097ad50ad
Clayton Pabst <claytonpabst@gmail.com>
2017-10-29 15:42:35 -0600

    bitmoji text is made just need the video address now.

--- src/components/Home/Bitmoji.js
+++ src/components/Home/Bitmoji.js
@@ -16,7 +16,16 @@ class Bitmoji extends Component {
   render() {
     return (
       <div className="bitmojiSection">
-
+        <div className='bitmojiImage'>
+          <img src="https://scontent.fslc1-1.fna.fbcdn.net/v/t1.0-9/22851711_829758373862462_3566424655210538394_n.jpg?oh=522fe6cdcc2573580e3259d0fa82ae11&oe=5A6BFC64" alt=""/>
+        </div>
+        <div className='bitmojiText'>
+          <p> &#160;&#160;&#160;&#160;Hi! I'm Clayton Pabst, welcome to my clone of Native Instruments.
+              Try out functionality by logging in as a user. <br/>username: user <br/>password: user
+              <br/><br/>&#160;&#160;&#160;&#160;Or log in as an admin and try out managing products in the mini CMS. <br/>
+              username: admin <br/> password: admin
+          </p>
+        </div>
       </div>
     );
   }
